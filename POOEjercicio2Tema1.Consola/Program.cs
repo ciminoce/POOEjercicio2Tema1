9f0e237e42b99834bff73ee0c199c8ad18ac1cd8
ciminoce <ciminoce@yahoo.com.ar>
2023-07-28 13:08:43 -0300

    Agregar archivos de proyecto.

---
+++ POOEjercicio2Tema1.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using POOEjercicio2Tema1.Entidades;
+
+namespace POOEjercicio2Tema1.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int dia = 29;
+            int mes = 2;
+            int anio = 2022;
+            if (ValidadorFechas.Validar(dia,mes, anio))
+            {
+                Console.WriteLine($"{dia:00}/{mes:00}/{anio:0000} es fecha válida");
+            }
+            else
+            { 
+                Console.WriteLine($"{dia:00}/{mes:00}/{anio:0000} no es fecha válida");
+            }
+        }
+    }
+}
---
+++ POOEjercicio2Tema1.Entidades/ValidadorFechas.cs
@@ -0,0 +1,32 @@
+﻿namespace POOEjercicio2Tema1.Entidades
+{
+    public static class ValidadorFechas
+    {
+        public static bool Validar(int dia, int mes, int anio)
+        {
+            if((dia>=1 && dia<=31)
+                && (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+                && (anio>0))
+            {
+                return true;
+            }
+            if((dia>=1 && dia<=30) 
+                && ((mes == 4 || mes == 6 || mes == 9 || mes == 11))
+                && (anio>0))
+            {
+                return true;
+            }
+            if((mes==2 && dia<=28) || (mes==2 && dia==29 && EsBisiesto(anio))) 
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+        private static bool EsBisiesto(int anio)
+        {
+            return anio%4 == 0 && (anio%100!=0 || anio%400==0);
+        }
+    }
+}
